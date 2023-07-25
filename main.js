5ac664a3fc2341c1ae6a07b526dfe6c65db37e76
Hadassah <faridathadassah4@gmail.com>
2023-07-25 11:26:28 +0100

    changes in the js code

--- main.js
+++ main.js
@@ -1,32 +1,44 @@
 const form = document.getElementById('form');
-const emailInput = document.getElementById('email');
+const emailInput = document.getElementById('email'); 
+const value = document.getElementById('value')
 const emailError = document.getElementById('emailError');
 const successMessage = document.getElementById('successMessage');
+const btn = document.getElementById('submit'); 
+const closeBtn = document.getElementById('dismissMessage');
+const email ='^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$';
 
-// const successMessage = document.getElementById('successMessage');
-// function display() {
-//     successMessage.innerHTML = ('successMessage')
-// }
+const displayError = () => { 
+  console.log('I want to display error')
+    emailError.innerHTML = 'Please enter a vaild email address';
+    // emailError.classList.toggle('hidden')
+    emailError.style.display = 'block'
+}
 
+const closeError = () => { 
+  console.log('I want to hide error')
+    emailError.innerHTML = '';
+    // emailError.classList.toggle('hidden')
+    emailError.style.display = 'hidden'
+}
 
-form.addEventListener('submit', function(event) {
+const submit = () => {
     event.preventDefault();
+    console.log('i am here')
+    console.log(emailInput.value)
+    // console.log(value)
+    
+    if (!emailInput.value.match(email)) {
+        displayError()
+    } else {
+        closeError()
+    }
+    value.innerText = emailInput.value
 
-if (!emailInput.validity.vaild) {
-    emailError.textContent = 'Please enter a vaild email address';
-    emailError.style.display = 'block';
-    successMessage.style.display = 'none'
-} else {
-    emailError.style.display = 'none';
-    successMessage.textContent = `Success! You've submitted the form with the email: ${emailInput.value}`;
-    successMessage.style.display = 'block'
-   form.reset(); 
-}   
-});
+    successMessage.style.display = "block";    
+    
+}
 
-emailInput.addEventListener('input', function() {
-    emailError.style.display = 'none';
-    successMessage.style.display = 'none' 
-});
-
-console.log('successMessage')
+closeBtn.onclick = () => {
+    successMessage.style.display = "none";
+  }
+btn.addEventListener('click', submit)
